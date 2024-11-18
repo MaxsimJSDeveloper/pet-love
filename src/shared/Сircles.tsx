d78ca23c43b2609037a8e522071a6a5aa7f9a3a3
Holovko Maksym <golovkomaksim852@gmail.com>
2024-11-18 20:29:59 +0200

    fix-circles

--- src/shared/Сircles.tsx
+++ src/shared/Сircles.tsx
@@ -5,10 +5,17 @@ interface CirclesProps {
 }
 
 const Circles = ({ children, onClick, isDisabled }: CirclesProps) => {
+  const handleClick = () => {
+    if (isDisabled) return;
+    onClick?.();
+  };
+
   return (
     <div
-      className="rounded-full border-[1px] border-[#262626] w-[40px] h-[40px] flex justify-center items-center"
-      onClick={onClick}
+      className={`rounded-full border-[1px] border-[#262626] w-[40px] h-[40px] flex justify-center items-center ${
+        isDisabled ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+      onClick={handleClick}
     >
       {children}
     </div>
